dc603245a96f75af4ca9382ce8e0d167fb2cbc50
orlandohuang <orlando.huang8@pm.me>
2021-11-30 12:50:26 +0700

    Added PowerBash logo and explanation.

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,24 @@
+# Importing needed packages
 import os
 import glob
 
+# Displaying PowerBash logo
+print("""
+██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░██████╗░░█████╗░░██████╗██╗░░██╗
+██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░██║
+██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╔╝██████╦╝███████║╚█████╗░███████║
+██╔═══╝░██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗██╔══██╗██╔══██║░╚═══██╗██╔══██║
+██║░░░░░╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██║░░██║██████╦╝██║░░██║██████╔╝██║░░██║
+╚═╝░░░░░░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝
+""")
 
+
+# Defining 'run_command()' function and make it run commands
 def run_command():
+    # 'exit' command
     if command == "exit":
         exit()
+    # 'ls' command
     elif command == "ls":
         path = input("Enter the path to list the folders and files in: ")
         if not path.endswith('/'):
@@ -15,10 +29,12 @@ def run_command():
         for file in glob.glob(f'{path}*'):
             if os.path.isfile(file):
                 print(file[len(path):])
+    # Error message when command not found
     else:
         print(f"Sorry, the command \"{command}\" is not recognised as a command!")
 
 
+# Allowing the users to run commands
 while True:
     command = input(">>> ")
     run_command()
