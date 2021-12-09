81fe1b83a054f5b8d8eb6ea703930f08a31df815
orlandohuang <orlando.huang8@pm.me>
2021-12-09 08:37:40 +0700

    Made code easier to recognize

--- scripts/main.py
+++ scripts/main.py
@@ -19,15 +19,19 @@ print("""
 
 # Defining 'run_command()' function and make it run commands
 def run_command():
+
     # 'exit' command
     if command == "exit":
         exit()
+
     # 'hostname' command
     elif command == "hostname":
         print(host_name)
+
     # 'hostip' command
     elif command == "hostip":
         print(host_ip)
+
     # 'ls' command
     elif command == "ls":
         path = input("Enter the path to list the folders and files in: ")
@@ -39,16 +43,19 @@ def run_command():
         for file in glob.glob(f'{path}*'):
             if os.path.isfile(file):
                 print(file[len(path):])
+
     # 'cp' or 'copy' command
     elif command == "cp" or command == "copy":
         original_path = input("Enter the path of the file you want to copy: ")
         copy_to = input("Enter the path where you want to copy it to: ")
         os.system(f'cp {original_path} {copy_to}')
+
     # 'mv' or 'move' command
     elif command == "mv" or command == "move":
         original_path = input("Enter the path of the file you want to move: ")
         move_to = input("Enter the path where you want to move it to: ")
         os.system(f'mv {original_path} {move_to}')
+
     # Error message when command not found
     else:
         print(f"Sorry, the command \"{command}\" is not recognised as a command!")
